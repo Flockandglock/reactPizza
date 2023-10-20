7cadea07dc1d6e9c80a8e2c61a14e9ef1ad8a7f1
Starukhin Denis <90477034+Flockandglock@users.noreply.github.com>
2023-10-20 12:37:53 +0400

    добавили useContext

--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -1,9 +1,17 @@
+import React, { useContext } from 'react';
+
+import {SearchContext} from '../../App';
+
 import styles from './search.module.scss';
 
 import clear from '../../assets/clear.svg'
 
 
-const Search = ({search, setSearch}) => {
+const Search = () => {
+
+    const {search, setSearch} = useContext(SearchContext);
+
+
     return (
         <div className={styles.root}>
             <svg className={styles.icon} xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 50 50" height="50px" id="Layer_1" version="1.1" viewBox="0 0 50 50" width="50px" ><rect fill="none" height="50" width="50"/><circle cx="21" cy="20" fill="none" r="16" stroke="#000000" stroke-linecap="round" stroke-miterlimit="10" stroke-width="2"/><line fill="none" stroke="#000000" stroke-miterlimit="10" stroke-width="4" x1="32.229" x2="45.5" y1="32.229" y2="45.5"/></svg>
